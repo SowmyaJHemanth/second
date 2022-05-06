c4bc65dce845bf1b1ee1f415aa80ca5b4027d59d
sowmya <sowmyapalhalli8@gmail.com>
2022-05-06 15:09:04 +0530

    my second project

---
+++ springgrps/src/main/java/springgrps/MainPrg.java
@@ -0,0 +1,24 @@
+package springgrps;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class MainPrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Resource resource = new ClassPathResource("ApplicationContext.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		Employee e1 = (Employee) factory.getBean("emp");
+		System.out.println(e1);
+		
+		Employee e2 = (Employee) factory.getBean("emp1");
+		System.out.println(e2);
+				
+	}
+
+}
